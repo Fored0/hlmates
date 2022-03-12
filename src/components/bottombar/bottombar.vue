96bd864c55a8b42b9bb066e3dc81564e19e29f24
Fored0 <2889745514@qq.com>
2022-03-12 17:27:59 +0800

    detail

--- src/components/bottombar/bottombar.vue
+++ src/components/bottombar/bottombar.vue
@@ -2,36 +2,69 @@
   <div class="bottom">
     <el-divider></el-divider>
     <div class="activeText">
-      <div class="text" @click="open(aboutUsText,aboutUsTitle)" >关于我们</div>
-      <div class="text" @click="open(concatUsText,concatUsTitle)">联系我们</div>
-      <div class="text" @click="open(statementText,statementTitle)">免责声明</div>
-      <div class="text" @click="open(portalText,portalTitle)">传送门</div>
+      <div class="text">
+        <el-popover
+          placement="top-start"
+          title="关于我们"
+          width="200"
+          trigger="hover"
+          content="我们来自于中国,立足于美利坚。"
+        >
+          <el-button type="text" slot="reference">关于我们</el-button>
+        </el-popover>
+      </div>
+      <div class="text">
+        <el-popover
+          placement="top-start"
+          title="联系我们"
+          width="200"
+          trigger="hover"
+          content="联系我们,官方Q群12452712。"
+        >
+          <el-button type="text" slot="reference">联系我们</el-button>
+        </el-popover>
+      </div>
+      <div class="text">
+        <el-popover
+          placement="top-start"
+          title="免责声明"
+          width="200"
+          trigger="hover"
+          content="责任与我无关。"
+        >
+          <el-button type="text" slot="reference">免责声明</el-button>
+        </el-popover>
+      </div>
+      <div class="text">
+        <el-popover
+          placement="top-start"
+          title="传送门"
+          width="200"
+          trigger="hover"
+          content="一道打开异世界的大门。"
+        >
+          <el-button type="text" slot="reference">传送门</el-button>
+        </el-popover>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'BottomBar',
-  data(){
-    return{
-      aboutUsText:'我们来自于中国,立足于美利坚',
-      aboutUsTitle:'关于我们',
-      concatUsText:'联系我们,官方Q群12452712',
-      concatUsTitle:'联系我们',
-      statementText:'责任与我无关',
-      statementTitle:'免责声明',
-      portalText:'一道打开异世界的大门',
-      portalTitle:'传送门'
-    }
+  name: "BottomBar",
+  data() {
+    return {
+     
+    };
   },
   methods: {
-    open(text,title) {
-      this.$alert(text, title,{
-        confirmButtonText: '确定'
+    open(text, title) {
+      this.$alert(text, title, {
+        confirmButtonText: "确定",
       });
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style scoped >
 .bottom {
